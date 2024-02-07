97d3e2e255a8ce68e6872deeb31a779871a3b217
rox-streuli <roxstreuli@gmail.com>
2024-02-07 19:24:00 +0000

    Add bounce_on_paddle() and bounce_on_wall() methods.

--- ball.py
+++ ball.py
@@ -7,3 +7,37 @@ class Ball(Turtle):
         self.shape('circle')
         self.penup()
         self.color('white')
+
+    def bounce_on_paddle(self):
+        direction = self.heading()
+        if direction < 90:
+            new_direction = 180 - direction
+        elif direction > 270:
+            diference_to_360 = 360 - direction
+            new_direction = 180 + diference_to_360
+        elif 180 > direction > 90:
+            diference_to_180 = 180 - direction
+            new_direction = diference_to_180
+        elif 180 < direction < 270:
+            diference_to_270 = 270 - direction
+            new_direction = 270 + diference_to_270
+        elif direction == 0:
+            new_direction = 180
+        elif direction == 180:
+            new_direction = 0
+        self.setheading(new_direction)
+
+    def bounce_on_wall(self):
+        direction = self.heading()
+        if 180 < direction < 270:
+            diference_to_180 = direction - 180
+            new_direction = 180 - diference_to_180
+        elif direction > 270:
+            diference_to_360 = 360 - direction
+            new_direction = diference_to_360
+        elif 90 < direction < 180:
+            diference_to_180 = 180 - direction
+            new_direction = 180 + diference_to_180
+        elif direction < 90:
+            new_direction = 360 - direction
+        self.setheading(new_direction)
