7e56e46fd6fce1336b2cd66e21a8d43d83195caa
rox-streuli <roxstreuli@gmail.com>
2024-02-10 12:44:45 +0000

    Refactor bouncce_on_wall() and bounce_on_paddle() methods.

--- ball.py
+++ ball.py
@@ -19,34 +19,22 @@ class Ball(Turtle):
 
     def bounce_on_paddle(self):
         direction = self.heading()
-        if direction < 90:
-            new_direction = 180 - direction
-        elif direction > 270:
-            diference_to_360 = 360 - direction
-            new_direction = 180 + diference_to_360
-        elif 180 > direction > 90:
-            diference_to_180 = 180 - direction
-            new_direction = diference_to_180
-        elif 180 < direction < 270:
-            diference_to_270 = 270 - direction
-            new_direction = 270 + diference_to_270
-        elif direction == 0:
+        if direction == 0:
             new_direction = 180
         elif direction == 180:
             new_direction = 0
+        elif 90 < direction < 180 or direction > 270:
+            new_direction = direction - 90
+        elif 180 < direction < 270 or direction < 90:
+            new_direction = direction + 90
         self.setheading(new_direction)
 
     def bounce_on_wall(self):
         direction = self.heading()
-        if 180 < direction < 270:
-            diference_to_180 = direction - 180
-            new_direction = 180 - diference_to_180
-        elif direction > 270:
-            diference_to_360 = 360 - direction
-            new_direction = diference_to_360
+        if direction < 90:
+            new_direction = 360 - direction
         elif 90 < direction < 180:
-            diference_to_180 = 180 - direction
-            new_direction = 180 + diference_to_180
-        elif direction < 90:
+            new_direction = direction + 90
+        elif direction > 180:
             new_direction = 360 - direction
         self.setheading(new_direction)
