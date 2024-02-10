6fe96596a84886d08b39b092502acc3009630fb2
rox-streuli <roxstreuli@gmail.com>
2024-02-10 17:33:05 +0000

    Refactor bounce_on_wall and bounce_on_paddle. Add self.setheading().

--- ball.py
+++ ball.py
@@ -7,15 +7,10 @@ class Ball(Turtle):
         self.shape('circle')
         self.penup()
         self.color('white')
+        self.setheading(10)
 
     def move(self):
         self.forward(20)
-
-    def detect_collision_with_wall(self):
-        pass
-
-    def detect_collision_with_paddle(self):
-        pass
 
     def bounce_on_paddle(self):
         direction = self.heading()
@@ -28,13 +23,19 @@ class Ball(Turtle):
         elif 180 < direction < 270 or direction < 90:
             new_direction = direction + 90
         self.setheading(new_direction)
+        self.move()
 
     def bounce_on_wall(self):
         direction = self.heading()
-        if direction < 90:
+        if direction == 90:
+            new_direction = 272
+        elif direction == 270:
+            new_direction = 92
+        elif direction < 90:
             new_direction = 360 - direction
         elif 90 < direction < 180:
             new_direction = direction + 90
         elif direction > 180:
             new_direction = 360 - direction
         self.setheading(new_direction)
+        self.move()
