bab3097a17a1435af7a17be60eb09282b6bd0924
rox-streuli <roxstreuli@gmail.com>
2024-02-15 18:58:10 +0000

    Refactored. Add import Board_line() class and create line in board.

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from turtle import Screen, Turtle
+from board_line import Board_line
 from paddle import Paddle
 from ball import Ball
 import time
 
 # constants
-LINE_SETUP_START = -300
 START_POSITION_PADDLE_1 = -400, 0
 START_POSITION_PADDLE_2 = 400, 0
 HEIGHT= 600
@@ -19,21 +19,8 @@ board.title("Pong")
 # turn off tracer
 board.tracer(0)
 
-# create board_line turtle
-board_line = Turtle()
-board_line.pensize(3)
-board_line.pencolor('white')
-board_line.hideturtle()
-board_line.penup()
-board_line.setposition(0, LINE_SETUP_START)
-board_line.setheading(90)
-
-# draw mid board line
-for _ in range(30):
-    board_line.pendown()
-    board_line.forward(10)
-    board_line.penup()
-    board_line.forward(10)
+# create line in the middle of the board
+line = Board_line()
 
 # crate paddles
 paddle_1 = Paddle(START_POSITION_PADDLE_1)
@@ -44,8 +31,8 @@ ball = Ball()
 
 # update screen to show board
 board.update()
-
 board.listen()
+
 # Collect key-events for paddle_1
 board.onkey(paddle_1.move_up, key='Up')
 board.onkey(paddle_1.move_down, key='Down')
