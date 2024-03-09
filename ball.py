2f9e26648c9250944ec3083d79b6b70f55191864
rox-streuli <roxstreuli@gmail.com>
2024-03-09 12:35:43 +0000

    Refactor bounce_on_paddle() and bounce_on_wall() methods.

--- ball.py
+++ ball.py
@@ -21,30 +21,3 @@ class Ball(Turtle):
 
     def bounce_on_paddle(self):
         self.x_move *= -1
-
-    # def bounce_on_paddle(self):
-    #     direction = self.heading()
-    #     if direction == 0:
-    #         new_direction = 180
-    #     elif direction == 180:
-    #         new_direction = 0
-    #     elif 90 < direction < 180 or direction > 270:
-    #         new_direction = direction - 90
-    #     elif 180 < direction < 270 or direction < 90:
-    #         new_direction = direction + 90
-    #     self.setheading(new_direction)
-
-    # My doce is not working as expected, used course solution
-    # def bounce_on_wall(self):
-    #     direction = self.heading()
-    #     if direction == 90:
-    #         new_direction = 272
-    #     elif direction == 270:
-    #         new_direction = 92
-    #     elif direction < 90:
-    #         new_direction = 360 - direction
-    #     elif 90 < direction < 180:
-    #         new_direction = direction + 90
-    #     elif direction > 180:
-    #         new_direction = 360 - direction
-    #     self.setheading(new_direction)
